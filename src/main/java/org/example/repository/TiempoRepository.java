10fa71074cb6a0915db8540577088bcb667ea1d4
swemmanuelgz <zundercoc@gmail.com>
2024-12-06 17:33:00 +0100

    repositorio creado

--- src/main/java/org/example/repository/TiempoRepository.java
+++ src/main/java/org/example/repository/TiempoRepository.java
@@ -1,10 +1,13 @@
 package org.example.repository;
 
-import java.net.MalformedURLException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
 
 import org.example.model.Tiempo;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class TiempoRepository {
@@ -14,15 +17,42 @@ public class TiempoRepository {
     private static final String API_KEY ="MWXPF8V59rh5BX8G983Bj4aWCm08aalDegD98fIKni4C2lK5jJnMIhA11lbde1MF";
 
         //Metodo para conseguir el Tiempo
-        public Tiempo getTiempo(String location) throws MalformedURLException{
+        public Tiempo getTiempo(String location) throws Exception{
 
             //construccion de la url
             String urlString = API_URL + "?location=" + location + "&API_KEY=" + API_KEY;
             URL url = new URL(urlString);
+            
+            //Configuración de la conexión
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("accept", "application/json");
 
+            //Vemos la respuesta que conseguimos 
+            BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            StringBuilder response = new StringBuilder();
+           //Leemos la respuesta y constuimos el objeto json
+            String line;
+            while ((line = br.readLine()) != null) {
+                response.append(line);
+            }
+            //Cerramos la conexion
+            br.close();
+            //Parseamos el Json que llega
             ObjectMapper mapper = new ObjectMapper();
+            JsonNode root = mapper.readTree(response.toString());
 
-            return new Tiempo();
+            //Cogemos los datos que queremos 
+            String estadoCielo = root.path("estado_cielo").asText();
+            String temperatura = root.path("temperatura").asText();
+            String viento = root.path("viento").asText();
+            String humedad = root.path("humedad").asText();
+            String coberturaNubosa = root.path("cobertura_nubosa").asText();
+
+            //Creamos el objeto Tiempo
+            Tiempo tiempo = new Tiempo(estadoCielo, temperatura, viento, humedad, coberturaNubosa);
+            
+            return tiempo;
         }
 
         
